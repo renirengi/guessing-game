f3f8b070793a5c3121c35a8f21c2d015bfcc07c6
Olga Bogdanova <TomieForever@gmail.com>
2021-09-05 16:29:23 +0300

    fix: desicion

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+     this.candidate = 0;  
+     this.max=0;
+     this.min=0;
+    }
 
-    setRange(min, max) {
+    
 
+    setRange(min, max) { 
+        this.max=max;
+        this.min=min;
     }
 
     guess() {
-
+        this.candidate = this.min + Math.round((this.max-this.min)/2);
+     return this.candidate;
     }
 
     lower() {
-
+      this.max = this.candidate; 
     }
 
     greater() {
-
+      this.min = this.candidate; 
     }
 }
 
